c9f995ad8fbe315ff3b53831e920974a3453893c
circlehotarux <mightyherox@gmail.com>
2021-02-04 18:09:48 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,6 @@
+# headless-crawler
+chrome headless crawler
+
+## 注释
+
+学习 NodeJS 课程项目，原项目：[Samaritan89 / headless-crawler](https://github.com/Samaritan89/headless-crawler)
---
+++ src/screenshot.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+const { screenshot } = require('./config/default');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.bilibili.com/');
+    await page.screenshot({
+        path: `${screenshot}/${Date.now()}.png`
+    });
+
+    await browser.close();
+})();
